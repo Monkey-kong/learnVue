df213ddfe29cfaaab9b98067b2862fdc1dc79241
lixianfu <lixianfu1@outlook.com>
2020-05-06 22:51:10 +0800

    vuex

---
+++ 04_vuex/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+store.commit('increment')
+
+console.log(store.state.count) // -> 1
